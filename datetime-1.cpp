ca44ebe0680ffa7d04e611e358371731336f0c55
poetild233 <1094324032@qq.com>
2020-03-30 20:46:48 +0800

    第三次的作业。

--- datetime-1.cpp
+++ datetime-1.cpp
@@ -37,6 +37,34 @@ DateTime::~DateTime()
 }
 void DateTime::showTime()
 {
-  printf("当前时间：%d/%d/%d %d:%d:%d\n", year, month, day, hour, minute, second);
+
+	int calendar[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
+	int lu_calendar[13] = {0,29,30,30,30,29,30,29,29,30,29,30,29};
+	
+	int lu_mon, lu_day;
+	int time = 0;
+	for(int i=0; i<month; i++) {
+		time += calendar[i];
+	} 
+	time += day;
+	if (time <= 24) {
+		lu_mon = 12;
+		lu_day = day - 1 + 7;
+	} else {
+		time -= 25;
+		int i = 1;
+		while(time > lu_calendar[i+1]) {
+			time -= lu_calendar[i];
+			i++;
+		}
+		if(i < 5) {
+			lu_mon = i;
+		} else {
+			lu_mon = i - 1;
+		}
+		lu_day = time;
+	}
+	
+	printf("Present lunar time: %d/%d %d:%d:%d:\n",lu_mon, lu_day, hour, minute, second);
 }
  
