3312c237856ada3a7f78904ad5a95e145245f80a
poetild233 <1094324032@qq.com>
2020-05-08 13:46:36 +0800

    用继承方法实现代码

--- SuperPrime2.cpp
+++ SuperPrime2.cpp
@@ -1,4 +1,4 @@
-//作业：面向对象设计以下框架的代码细节，程序能编译运行得到正确结果 
+//作业：删除多余的集合类，使用继承方法实现相同的功能。 
 #include <iostream>
 class Prime {
   public:
@@ -8,125 +8,110 @@ class Prime {
 	}
 	~Prime() {
 	}
-  	bool isPrime() { 
-  	  //2到number-1的因子 
-  	  for(int i=2;i<number;i++) {
-  	  	if(number%i == 0)
-  	  	return false;
-		}
-  	  return true;
+ 	bool isPrime() { 
+  	  //2到number-1的因子
+		 for(int i=2; i<number; i++) {
+	  	if(number % i == 0 && i != number -1)
+	  		return false;
+	  } 
+  	  return true; 
+	}
+	int getnum() {
+		return number;
 	}
   private:
   	const int number;
-  	friend class PrimeSet; 
-  	friend class SuperPrimeSet;
 }; 
-class PrimeSet {
-  public:
-  	PrimeSet(int size) {
-  	  //集合的构造什么？ 
-  	  N = new Prime*[size];         //size个指向prime的指针 
-  	  this->size = size;
-  	  index = 0;
-	}
-	~PrimeSet() {
-  	  for (int i = 0; i < index; ++i)  //销毁对象 
-		delete N[i]; 
-	  delete[] N;
-	}
-	bool add(int n) {
-	  if(index == size)  return false;
-	  Prime *p = new Prime(n);
-	  N[index] = p;
-	  index += 1;
-	  return true;
-	}
-	int sum() {
-		int sum=0;
-		for(int i=0;i<size;i++) {
-			sum += N[i]->number;
-		}
-	  return sum;
-	}
-	int multi() {
-		int multi = 1;
-		for(int i=0;i<size;i++) {
-			multi *= N[i]->number;
-		}
-	  return multi;
-	}
-	int squareSum() {
-		int square = 0;
-		for(int i=0;i<size;i++) {
-			square += (N[i]->number * N[i]->number);
-		}
-	  return square;
-	}
-	bool isAllPrime() {
-	  for(int i = 0; i < index; i++)
-	    if (!N[i]->isPrime())
-	      return false;
-	  return true;
-	} 
-  private:
-  	Prime **N;
-	int size, index;
-};
 class SuperPrime : public Prime {
   public:
-  	SuperPrime():Prime(0),pset(3) {  //为什么必须有？ 
+  	SuperPrime():Prime(0),index(0) {  //为什么必须有？ 
+  		
   	}
-  	SuperPrime(int n):Prime(n),pset(3) {
+  	SuperPrime(int n):Prime(n),index(0) {
 	  // number split into N
 	  int temp = n;
 	  while(temp > 0) {
 	  	int t = temp % 10;
 	  	temp /= 10;
-	  	pset.add(t);  //作业：单个数字为对象？还是和/积/平方和为对象？ 
+	    pset[index]= new Prime(t);
+	    index++;
 	  } 
 	}
   	~SuperPrime() {
 	}
-  	bool isSuperPrime() {
-	  if (Prime::isPrime() && pset.isAllPrime())
+  	bool isPrime() {   //类/对象的接口，更抽象说是外观 
+	  if (Prime::isPrime() && isAllPrime())
 	    return true; 
   	  return false;
 	}
+
   private:
-  	PrimeSet pset; 
+  	Prime *pset[];
+  	int index;
+	int sum() {
+	  int sum = 0;
+	  for(int i=0;i<index;i++) {
+	  	sum += pset[i]->getnum();
+	  }
+	  return sum;
+	}
+	int multi() {
+	  int multi=1;
+	  for(int i=0;i<index;i++) {
+	  	multi *= pset[i]->getnum();
+	  }
+	  return multi;
+	}
+	int squareSum() {
+	  int squaresum=0;
+	  for(int i=0;i<index;i++) {
+	  	squaresum += pset[i]->getnum() * pset[i]->getnum();
+	  }
+	  return squaresum;
+	}
+	bool isAllPrime() {
+		Prime a(sum()); Prime b(multi()); Prime c(squareSum());
+		if(a.isPrime() && b.isPrime() && c.isPrime())
+	      return true;
+	  return false;
+	} 
 };
-class SuperPrimeSet {
+class PrimeSet {
   public:
-  	SuperPrimeSet(int from, int to) {
-  	  size = to - from;
-  	  for (int i = from; i < to; i++)
-  	    set[i-from] = new SuperPrime(i);
+  	PrimeSet(int size) {
+  	  //集合的构造什么？ 
+  	  for(int i=0;i<size;i++) {
+  	  	set[i] = new SuperPrime(100+i);
+		}
+  	  this->size = size;
+  	  index = 0;
 	}
-  	~SuperPrimeSet() {
-  	  for(int i = 0; i < size; i++)
-  	    delete set[i];
+	~PrimeSet() {
+  	  for (int i = 0; i < index; ++i)  //销毁对象 
+		delete set[i]; 
 	}
-  	int count() {
+ 	int count() {
   	  int count = 0;
   	  for (int i = 0; i < size; i++)
-  	    if(set[i]->isSuperPrime())
+  	    if(set[i]->isPrime())
   	      count += 1;
 	  return count; 
 	}
-  	int sum() {
-  	  int sum = 0;
-  	  for (int i = 0; i < size; i++)
-  	    if(set[i]->isSuperPrime())
-  	      sum += set[i]->number;
-	  return sum; 
+	int sum() {
+		int sum=0;
+		for(int i = 0; i<size; i++ ) {
+			if(set[i]->isPrime())
+			sum += set[i]->getnum();
+		}
+		return sum;
 	}
   private:
-  	SuperPrime **set;
-  	int size, index;
+  	SuperPrime *set[];
+	int size, index;
 };
 int main() {
   SuperPrime sp(113);
-  if (sp.isSuperPrime())
+  if (sp.isPrime())
     std::cout << "113 is SuperPrime" << std::endl;
   else
     std::cout << "113 is NOT SuperPrime" << std::endl;
